e23d23470bcf3d216eee5e49865092d1780d945e
waimanhou <38213872+waimanhou@users.noreply.github.com>
2022-12-01 23:45:32 +0800

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,9 +40,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "edcbfb619e59137ec8705c3ade45e149",
 "icons/Icon-maskable-192.png": "d9a551985e91e5d65a5eb696ac43f238",
 "icons/Icon-maskable-512.png": "edcbfb619e59137ec8705c3ade45e149",
-"index.html": "9a9437b90bcf81f939fc630a60bd4b56",
-"/": "9a9437b90bcf81f939fc630a60bd4b56",
-"main.dart.js": "96ef37485f33f55c01e1486233657234",
+"index.html": "1b5983701de5f88b048be3b4949d4732",
+"/": "1b5983701de5f88b048be3b4949d4732",
+"main.dart.js": "6d702344ec1900f51e5814c081b51744",
 "manifest.json": "1a31d551f870f7b5ed267903476e79a6",
 "version.json": "ee30dda3192f88376040c466d75ed3b7"
 };
